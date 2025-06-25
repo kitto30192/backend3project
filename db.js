1bc2b6651a1e7e43feef121f153cd0ccd54bd2fd
kitto30192 <Kittopra40@gmail.com>
2025-06-25 11:38:02 +0530

    vercel update 3

--- db.js
+++ db.js
@@ -2,23 +2,21 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
-
-// models/WebsiteTrack.js
-
+if (mongoose.connection.readyState === 0) {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+}
 
 const websiteTrackSchema = new mongoose.Schema({
   url: String,
   shorturl: Number
 });
 
-// ✅ Correct in ESM
-const WebsiteTrack = mongoose.model('WebsiteTrack', websiteTrackSchema, 'websiteTrack');
+const WebsiteTrack = mongoose.models.WebsiteTrack || mongoose.model('WebsiteTrack', websiteTrackSchema, 'websiteTrack');
 
 
 
